8045787432abaacb44e5f8483465be699cd0e6fc
Rhys <Rhys26@hotmail.com>
2019-08-15 17:46:55 +0100

    Added Pause menu. Added Continue, Main menu and exit buttons for pause menu. Pause menu locks and unlocks mouse depending on the running state of the game.

--- Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/MouseLook.cs
+++ Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/MouseLook.cs
@@ -57,12 +57,6 @@ namespace UnityStandardAssets.Characters.FirstPerson
 
         public void SetCursorLock(bool value)
         {
-            lockCursor = value;
-            if(!lockCursor)
-            {//we force unlock the cursor if the user disable the cursor locking helper
-                Cursor.lockState = CursorLockMode.None;
-                Cursor.visible = true;
-            }
         }
 
         public void UpdateCursorLock()
@@ -74,25 +68,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
 
         private void InternalLockUpdate()
         {
-            if(Input.GetKeyUp(KeyCode.Escape))
-            {
-                m_cursorIsLocked = false;
-            }
-            else if(Input.GetMouseButtonUp(0))
-            {
-                m_cursorIsLocked = true;
-            }
-
-            if (m_cursorIsLocked)
-            {
-                Cursor.lockState = CursorLockMode.Locked;
-                Cursor.visible = false;
-            }
-            else if (!m_cursorIsLocked)
-            {
-                Cursor.lockState = CursorLockMode.None;
-                Cursor.visible = true;
-            }
+            
         }
 
         Quaternion ClampRotationAroundXAxis(Quaternion q)
---
+++ Assets/Standard Assets/UI/PauseMenu.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+
+    public void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Level.IsGameRunning)
+            {
+                Pause();
+            }
+            else
+            {
+                Continue();
+            }
+        }
+    }
+
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Level.IsGameRunning = false;
+
+        Time.timeScale = 0f;
+
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = true;
+    }
+
+    public void Continue()
+    {
+        pauseMenu.SetActive(false);
+        Level.IsGameRunning = true;
+        Time.timeScale = 1f;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}

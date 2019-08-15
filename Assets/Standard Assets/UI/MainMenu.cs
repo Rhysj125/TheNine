9c92d20d3fa3ef7ce62602217c8f08eb88356241
Rhys <Rhys26@hotmail.com>
2019-08-15 17:47:21 +0100

    Add Main menu and Option Menu

---
+++ Assets/Standard Assets/UI/MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject optionsMenu;
+
+    public void PlayGame()
+    {
+        ResetAllToDefault();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void GoToOptions()
+    {
+        gameObject.SetActive(false);
+        optionsMenu.SetActive(true);
+    }
+
+    private void ResetAllToDefault()
+    {
+        Level.ResetToDefault();
+        Player.GetInstance().ResetToDefault();
+    }
+}
---
+++ Assets/Standard Assets/UI/Options.cs
@@ -0,0 +1,20 @@
+﻿using Game.Settings;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Options : MonoBehaviour
+{
+    public GameObject mainMenu;
+
+    public void ToggleEnemySpawning(Toggle toggle)
+    {
+        Settings.SpawnEnemies = toggle.isOn;
+        Debug.Log(Settings.SpawnEnemies);
+    }
+
+    public void ReturnMainMenu()
+    {
+        mainMenu.SetActive(true);
+        this.gameObject.SetActive(false);
+    }
+}
